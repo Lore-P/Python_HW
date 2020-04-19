8da3b416a261a6d65494b1c8ab7aa5bb2a356e96
Lore-P <lore.peeters2@gmail.com>
2020-04-19 20:07:27 +0100

    first commit

---
+++ Fibonacci_For_Loop/Fib_For.py
@@ -0,0 +1,15 @@
+#Python.exe C:\Dev\intro\GitHub\HW_4\Fibonacci_For_Loop\Fib_For.py
+
+# index number that will be run through the formula
+fib_index = 3
+range = []
+
+if x <= fib_index:
+    
+for x in range:
+    temp = []
+    temp[i] = temp.append(x + temp[i-1])
+    fib_no_at_index = temp[fib_index]
+    
+print(fib_no_at_index)  
+    
