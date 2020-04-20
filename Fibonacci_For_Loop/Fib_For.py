f01cb97b815aabbbacfaed1604dd6c80d25e620b
Lore-P <lore.peeters2@gmail.com>
2020-04-21 00:05:57 +0100

    Fibonacci numbers

--- Fibonacci_For_Loop/Fib_For.py
+++ Fibonacci_For_Loop/Fib_For.py
@@ -1,16 +1,17 @@
-#Python.exe C:\Dev\intro\GitHub\HW_4\Fibonacci_For_Loop\Fib_For.py
+#Python.exe C:\Dev\intro\GitHub\HW_4\Python_HW3\Fibonacci_For_Loop\Fib_For.py
 
 # index number that will be run through the formula
-fib_index = 3
-range = []
+fib_index = 5
+fib_no_at_index = [0,1]
 
-if x <= fib_index:
-    range[i] = range.append(x)
-    
-for x in range:
-    temp = []
-    temp[i] = temp.append(x + temp[i-1])
-    fib_no_at_index = temp[fib_index]
-    
-print(fib_no_at_index)  
-    
+#create Fibonacci For-loop
+for number in range(3,fib_index):
+    if number == 0:
+        fib_no_at_index[0]
+    elif number == 1:
+        fib_no_at_index[1]
+    else:    
+        new_number = fib_no_at_index[number-3] + fib_no_at_index[number-2]
+        fib_no_at_index.append(new_number)
+print(fib_no_at_index)
+
--- Fibonacci_For_Loop/Fib_While.py
+++ Fibonacci_For_Loop/Fib_While.py
@@ -1,9 +1,16 @@
-
+#Python.exe C:\Dev\intro\GitHub\HW_4\Python_HW3\Fibonacci_For_Loop\Fib_While.py
 
 fib_index = 3
+number = 0
+fib_no_at_index = [0, 1]
 
-while x <= fib_index:
-	temp = []
-        temp[i] = temp.append(x + temp[i-1])
-	x += 1
-    fib_no_at_index = temp[fib_index]
+if number == 0:
+    fib_no_at_index[0]
+elif number == 1:
+    fib_no_at_index[1]
+else:
+    number = fib_index
+    while number <= fib_index:
+        new_number = fib_no_at_index[number-3] + fib_no_at_index[number-2]
+        fib_no_at_index.append(new_number)
+        number += 1
